9f6f7245282078082f2b918e7ac4239a66fb901e
MattIPv4 <matthew@cowley.org.uk>
2019-05-16 15:07:43 +0100

    Use cookies to store data, validate tz before generation

--- script.js
+++ script.js
@@ -3,26 +3,33 @@ document.addEventListener('DOMContentLoaded', function () {
     moment.tz.setDefault(moment.tz.guess());
 
     // Initial data
-    var times = [
-        ["Europe/London", "London"],
-        ["America/New_York", "New York"],
-        ["America/Los_Angeles", "San Francisco"],
-    ], idCounter = 0;
+    var times = {
+        0: ["Europe/London", "London"],
+        1: ["America/New_York", "New York"],
+        2: ["America/Los_Angeles", "San Francisco"],
+    };
+
+    // Attempt to fetch times data from cookie, write back
+    var cookieData = Cookies.getJSON('times');
+    if (cookieData) {
+        times = cookieData;
+    }
+    Cookies.set('times', times, {expires: 3650});
 
     // Create a new section
-    function createSection(data) {
+    function createSection(id, data) {
         var section = document.createElement("section");
-        section.setAttribute("id", "times-" + idCounter.toString());
-        idCounter++;
+        section.setAttribute("id", "times-" + id);
         section.setAttribute("data-tz", data[0]);
         section.setAttribute("data-label", data[1]);
-        document.body.appendChild(section);
+        document.querySelector("main").appendChild(section);
         return section;
     }
 
     // Build initial sections from data
-    for (var i = 0; i < times.length; i++) {
-        createSection(times[i]);
+    for (var key in times) {
+        if (!times.hasOwnProperty(key)) continue;
+        createSection(key, times[key])
     }
 
     setInterval(function () {
@@ -31,46 +38,60 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Loop over all sections
         for (var i = 0; i < sections.length; i++) {
+            // Get/create elements
             var elm = sections[i];
-
-            // Set the label
+            var div = elm.querySelector("div");
+            if (!div) {
+                div = document.createElement("div");
+                elm.appendChild(div);
+            }
             var h1 = elm.querySelector("h1");
             if (!h1) {
                 h1 = document.createElement("h1");
                 elm.appendChild(h1);
             }
-            h1.textContent = elm.getAttribute("data-label");
-
-            // Do time
-            var theMoment = moment().tz(elm.getAttribute("data-tz"));
             var h2 = elm.querySelector("h2");
             if (!h2) {
                 h2 = document.createElement("h2");
                 elm.appendChild(h2);
             }
-
-            // Set the time
             var span = h2.querySelector("span");
             if (!span) {
                 span = document.createElement("span");
                 h2.appendChild(span);
             }
-            span.textContent = theMoment.format("hh:mm:ss a");
-
-            // Set the tz
             var small = h2.querySelector("small");
             if (!small) {
                 small = document.createElement("small");
                 h2.appendChild(small);
             }
-            small.textContent = " (" + theMoment.format("z Z") + ")";
-
-            // Set the date
             var h3 = elm.querySelector("h3");
             if (!h3) {
                 h3 = document.createElement("h3");
                 elm.appendChild(h3);
             }
+
+            // Set the label
+            h1.textContent = elm.getAttribute("data-label");
+
+            // Validate tz & generate moment
+            var tz = elm.getAttribute("data-tz");
+            if (moment.tz.zone(tz) === null) {
+                elm.className = "error";
+                span.textContent = "";
+                small.textContent = "Timezone '" + tz + "' not found";
+                h3.textContent = "";
+                continue;
+            }
+            var theMoment = moment().tz(tz);
+
+            // Set the time
+            span.textContent = theMoment.format("hh:mm:ss a");
+
+            // Set the tz
+            small.textContent = " (" + theMoment.format("z Z") + ")";
+
+            // Set the date
             h3.textContent = theMoment.format("dddd Do MMMM");
 
             // Do snooze/awake
@@ -79,11 +100,9 @@ document.addEventListener('DOMContentLoaded', function () {
                 beforeTime = moment('08:00:00', format),
                 afterTime = moment('22:00:00', format);
             if (time.isBetween(beforeTime, afterTime)) {
-                elm.classList.add("awake");
-                elm.classList.remove("asleep");
+                elm.className = "awake";
             } else {
-                elm.classList.add("asleep");
-                elm.classList.remove("awake");
+                elm.className = "asleep";
             }
         }
     }, 100);
